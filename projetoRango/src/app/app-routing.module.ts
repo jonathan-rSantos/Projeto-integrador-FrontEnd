48e6a163a6b975beaf972646767622e62114dad9
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-05-28 16:56:45 -0300

    Tudo ok

--- projetoRango/src/app/app-routing.module.ts
+++ projetoRango/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ContatoComponent } from './contato/contato.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'inicio', pathMatch: 'full'},  /* adicionar tela principal (entrar ou login) */
   {path: 'inicio', component: InicioComponent},
+  {path:'contato', component: ContatoComponent},
   {path: 'cadastrar', component: CadastrarComponent}
 ];
 
