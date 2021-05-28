7a06f9eb0b77d0ace75a1c9f7bba620be2a2fa5b
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-05-28 14:06:37 -0300

    Component cadastrar

--- projetoRango/src/app/app-routing.module.ts
+++ projetoRango/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'cadastrar', pathMatch: 'full'},  /* adicionar tela principal (entrar ou login) */
+  /* {path: 'entrar', component: EntrarComponent }, após componente entrar descomentar*/
+  {path: 'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
