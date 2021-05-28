bee51548e7afa18e25cc3175abea1890e39f64e0
MayaraDantas <mayarau.ugatinha25@gmail.com>
2021-05-28 16:07:51 -0300

    primara pagina contato

--- projetoRango/src/app/app-routing.module.ts
+++ projetoRango/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'contato', pathMatch:'full'},
+  {path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
