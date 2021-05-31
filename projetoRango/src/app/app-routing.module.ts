9d6b04fa1ea5e338b5ba54baefff045234fa5db0
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-05-31 17:38:12 -0300

    Tela de login

--- projetoRango/src/app/app-routing.module.ts
+++ projetoRango/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'cadastrar', pathMatch: 'full'},  /* adicionar tela principal (entrar ou login) */
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'entrar', component: EntrarComponent}
 ];
 
 @NgModule({
