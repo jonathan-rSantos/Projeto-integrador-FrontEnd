0b1c246bf7e5052a3784005ebc1c0e26d0c93d3f
Amanda <anoyori@gmail.com>
2021-06-02 10:27:31 -0300

    Implementação do service-auth

--- projetoRango/src/environments/environment.prod.ts
+++ projetoRango/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  token: '',
+  nome: '',
+  id: 0,
+  foto: '',
+  server: 'https://projetorango.herokuapp.com'
 };
+
+//http://localhost:8080
