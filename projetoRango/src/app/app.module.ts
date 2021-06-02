9bfa5daed7ffea7f864e4a6c00db1dff1294af99
Amanda <anoyori@gmail.com>
2021-06-02 11:26:01 -0300

    Implementação do consumo do cadastro-login

--- projetoRango/src/app/app.module.ts
+++ projetoRango/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { ContatoComponent } from './contato/contato.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { EntrarComponent } from './entrar/entrar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projetoRango/src/app/model/UserLogin.ts
+++ projetoRango/src/app/model/UserLogin.ts
@@ -7,3 +7,4 @@ export class UserLogin{
     public tipo: string
     public token: string
 }
+
