aa1a7842d2a4a0f8614e313a1730aed26cca9515
Amanda <anoyori@gmail.com>
2021-06-02 13:31:38 -0300

    Model de user

--- projetoRango/src/app/cadastrar/cadastrar.component.ts
+++ projetoRango/src/app/cadastrar/cadastrar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserLogin } from '../model/UserLogin';
+import { User } from '../model/User';
 import { AuthService } from '../service/auth.service';
 
 @Component({
