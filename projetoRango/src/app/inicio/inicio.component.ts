0963ac30d6b6dbf854a094a3feae5895546ce86b
coder samuel <samuelsantos.ss95@gmail.com>
2021-06-09 11:35:03 -0300

    Post-get-postagem Samuca

--- projetoRango/src/app/inicio/inicio.component.ts
+++ projetoRango/src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,9 +13,19 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  tema: Tema = new Tema()
+  postagem: Postagem = new Postagem()
+  listaTemas: Tema []
+  idTema: number
+  
+  user: User = new User()
+  idUser = environment.id
 
-  constructor(  
-    private router: Router
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -22,6 +37,19 @@ export class InicioComponent implements OnInit {
   teste(){
     console.log('funcionei')
   }
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
 
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
+  }
 }
 
