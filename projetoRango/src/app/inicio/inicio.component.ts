0147661bc7154ee6608f8427c669570213ebf10e
coder samuel <samuelsantos.ss95@gmail.com>
2021-06-10 14:14:11 -0300

    Ajustes finais - adm

--- projetoRango/src/app/inicio/inicio.component.ts
+++ projetoRango/src/app/inicio/inicio.component.ts
@@ -27,7 +27,7 @@ export class InicioComponent implements OnInit {
     private router: Router,
     private postagemService: PostagemService,
     private temaService: TemaService,
-    private authService: AuthService
+    public authService: AuthService
   ){}
   
   ngOnInit() {
